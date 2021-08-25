bbaa9402484c46efbc03d52abb9b9a7cee962779
David Weinstein <67246404+dweinteinb@users.noreply.github.com>
2021-08-24 23:06:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 import streamlit as st
+import pandas as pd
+from pytrends.request import TrendReq
 
-st. title('COVID statistics in the USA')
-#st. subheader('Predict the epidemiology and vaccination rate, and the anti-covid twits')
-#st. text('select a state')
-#st. text('select anti-covid twits')
+header = st.beta_container()
+map_usa = st.beta_container()
+
+with header:
+    st. title('COVID statistics in the USA')
+    #st. subheader('Predict the epidemiology and vaccination rate, and the anti-covid twits')
+    st. text('select a state')
+
+    imput_feature = st.text_input ('Keyword','anti-vax')
+
+# pytrends
+pytrends = TrendReq(hl='en-US')
+kw_list =
+cat = '0' # as string -- no subcat, 
+pytrends.build_payload (kw_list ,
+                 cat ,
+     timeframe ='today 3-m', 
+     geo='',
+     gprop='')
+
+st.write(imput_feature)
+
